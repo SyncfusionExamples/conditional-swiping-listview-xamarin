ceae720d2bd4800fe76df78d353f56e6f373074b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-06-24 12:41:31 +0530

    XAMARIN-34608 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfListView sfListView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            sfListView = bindable.FindByName<SfListView>("listView");
+            sfListView.SwipeStarted += SfListView_SwipeStarted;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void SfListView_SwipeStarted(object sender, Syncfusion.ListView.XForms.SwipeStartedEventArgs e)
+        {
+            if((e.ItemData as Contacts).BackgroundColor == Color.LightGray)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            sfListView.SwipeStarted -= SfListView_SwipeStarted;
+            sfListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
